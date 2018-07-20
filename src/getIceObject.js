962ffd0ec11c94b1abbdf6313975a23eb26610c4
GLODEIT\Ha.TN <ha.tn@globaldesignit.vn>
2018-07-20 16:09:04 +0700

    webrtc

--- src/getIceObject.js
+++ src/getIceObject.js
@@ -7,10 +7,10 @@ function getIceObject(cb) {
             ident: "hatran92",
             secret: "f4e63042-8bd2-11e8-a73d-4792a42eb3fb",
             channel : "hatn207.github.io",
-            // domain: "hatn207.github.io",
+            domain: "hatn207.github.io",
             // application: "default",
-            // room: "default",
-            // secure: 1,
+            room: "default",
+            secure: 1,
         },
         success: function (data, status) {
             // data.d is where the iceServers object lives
