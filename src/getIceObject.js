9058d86f179c4920c255b3b76f2dd4f33e251469
GLODEIT\Ha.TN <ha.tn@globaldesignit.vn>
2018-07-20 15:17:01 +0700

    webrtc

--- src/getIceObject.js
+++ src/getIceObject.js
@@ -2,7 +2,7 @@ const $ = require('jquery');
 
 function getIceObject(cb) {
     $.ajax({
-        url: "https://global.xirsys.net",
+        url: "https://service.xirsys.com/ice",
         data: {
             ident: "hatran92",
             secret: "f4e63042-8bd2-11e8-a73d-4792a42eb3fb",
@@ -14,7 +14,6 @@ function getIceObject(cb) {
         success: function (data, status) {
             // data.d is where the iceServers object lives
             cb(data.d);
-            console.log(data);
         },
     });
 }
